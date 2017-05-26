bd502efc99bb2f892c7a4d9d31eda3691f3ba365
tonylau <670292548@qq.com>
2017-05-26 10:03:23 +0800

    1.0.22 - Fixed reconnecting issue.

--- chatease.js
+++ chatease.js
@@ -542,7 +542,11 @@ chatease.version = '1.0.22';
 		CHATEASE_VIEW_SEND: 'chateaseViewSend',
 		CHATEASE_VIEW_PROPERTY: 'chateaseViewProperty',
 		CHATEASE_VIEW_CLEARSCREEN: 'chateaseViewClearScreen',
-		CHATEASE_VIEW_NICKCLICK: 'chateaseViewNickClick'
+		CHATEASE_VIEW_NICKCLICK: 'chateaseViewNickClick',
+		
+		// Timer Events
+		CHATEASE_TIMER: 'chateaseTimer',
+		CHATEASE_TIMER_COMPLETE: 'chateaseTimerComplete'
 	};
 })(chatease);
 
@@ -664,6 +668,63 @@ chatease.version = '1.0.22';
 				}
 			}
 		}
+	};
+})(chatease);
+
+(function(chatease) {
+	var utils = chatease.utils,
+		events = chatease.events;
+	
+	utils.timer = function(delay, repeatCount) {
+		var _this = utils.extend(this, new events.eventdispatcher('utils.timer')),
+			_intervalId,
+			_currentCount = 0,
+			_running = false;
+		
+		function _init() {
+			_this.delay = delay || 50;
+			_this.repeatCount = repeatCount || 0;
+		}
+		
+		_this.start = function() {
+			if (_running === false) {
+				_intervalId = setInterval(_onTimer, _this.delay);
+				_running = true;
+			}
+		};
+		
+		function _onTimer() {
+			_currentCount++;
+			_this.dispatchEvent(events.CHATEASE_TIMER);
+			
+			if (_this.repeatCount > 0 && _currentCount >= _this.repeatCount) {
+				_this.stop();
+				_this.dispatchEvent(events.CHATEASE_TIMER_COMPLETE);
+			}
+		}
+		
+		_this.stop = function() {
+			if (_running) {
+				clearInterval(_intervalId);
+				_intervalId = 0;
+				_running = false;
+			}
+		};
+		
+		_this.reset = function() {
+			_this.stop();
+			_currentCount = 0;
+		};
+		
+		_this.currentCount = function() {
+			return _currentCount;
+		};
+		
+		_this.running = function() {
+			return _running;
+		};
+		
+		_init();
 	};
 })(chatease);
 
